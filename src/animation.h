95318d9409c2c3904bd070490c7a92471e191e17
Yago Guirra <yagoguirra@hotmail.com>
2025-01-28 16:27:07 -0300

    main quase finalizada, alguns pontos atualizados.

--- src/animation.h
+++ src/animation.h
@@ -9,20 +9,17 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 5
 #define MATRIX_DEPTH 3
-#define MIAW_FRAME_COUNT 8
-#define MIAW_FRAME_WIDTH 5
-#define MIAW_FRAME_HEIGHT 5
-#define SPIRAL_FRAME_COUNT 50
-#define SPIRAL_FRAME_WIDTH 5
-#define SPIRAL_FRAME_HEIGHT 5
 
 // exemplo de como fazer o define para a qt de frames
 
 #define NUM_PIXELS 25 // Número de LEDs na matriz
-#define FAN_FRAME_COUNT 4
-#define NEW_PISKEL_FRAME_COUNT 6
-#define NEW_PISKEL_FRAME_WIDTH 5
-#define NEW_PISKEL_FRAME_HEIGHT 5
+#define FAN_FRAME_COUNT 5
+#define HEART_FRAME_COUNT 8
+#define VOWELS_FRAME_COUNT 6
+#define COLOR_FRAME_COUNT 6
+#define MIAW_FRAME_COUNT 8
+#define SPIRAL_FRAME_COUNT 50
+#define BOMB_FRAME_COUNT 5
 
 // Funções
 void convertToRGB(int argb, int rgb[3]); //apesar da sugestão, optei por deixar deste modo, por argb ser um pouco mais completo para converssão.
@@ -32,10 +29,11 @@ void display_animation(const uint32_t frames[][NUM_PIXELS], uint num_frames, uin
 // Variáveis globais
 // exemplo de como fazer
 
-extern const uint32_t fan_data[4][25];
-extern const uint32_t color_data[6][25];
-extern const uint32_t vowels_data[5][25];
-extern const uint32_t miaw_data[8][25];
-extern const uint32_t spiral_data[50][25];
-
+extern const uint32_t fan_data[5][25];      // implemendado
+extern const uint32_t heart_data[8][25];    // implemendado
+extern const uint32_t color_data[6][25];    // implemendado
+extern const uint32_t vowels_data[5][25];   // implemendado
+extern const uint32_t miaw_data[8][25];     // implemendado
+extern const uint32_t spiral_data[50][25];  // implemendado
+extern const uint32_t bomb_data[5][25];
 #endif // ANIMATIONS_H
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
 
 #include "include/buzzer.h"
 #include "include/keypad.h"
@@ -7,6 +8,7 @@
 
 #include "animation.h"
 
+#define ANIMATION_TIME 3000
 #define DEBUG(var) printf("%s : %c\n", #var, var)
 
 int main() {
@@ -20,62 +22,146 @@ int main() {
     while (true) {
         char key_pressed = keypad_return();
         if(key_pressed != ' ') {
+
+            uint16_t time = 0;
+
             DEBUG(key_pressed);
 
             switch (key_pressed) {
-                case '1':
-                #if 1
-                    display_animation(
-                        fan_data, 
-                        FAN_FRAME_COUNT, 
-                        3000
-                    );
-                #endif
+                // Animação 1: ventilador
+                case '1':             
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            fan_data, 
+                            FAN_FRAME_COUNT, 
+                            1000
+                        );
+                        time += 1000;
+                    }
                     break;
+
+                // Animação 2: coração
                 case '2':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            heart_data, 
+                            HEART_FRAME_COUNT, 
+                            1000
+                        );
+                        time += 1000;
+                    }
                     break;
+
+                // Animação 3: cores
                 case '3':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            color_data, 
+                            COLOR_FRAME_COUNT, 
+                            1000
+                        );
+                        time += 1000;
+                    }
                     break;
+
+                // Animação 4: vogáis
                 case '4':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            vowels_data, 
+                            VOWELS_FRAME_COUNT, 
+                            3000
+                        );
+                        time += 3000;
+                    }
                     break;
+
+                // Animação 5: gatos
                 case '5':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            miaw_data, 
+                            MIAW_FRAME_COUNT, 
+                            3000
+                        );
+                        time += 3000;
+                    }
                     break;
+
+                // Animação 6: espiral
                 case '6':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            spiral_data, 
+                            SPIRAL_FRAME_COUNT, 
+                            3000
+                        );
+                        time += 3000;
+                    }
                     break;
+
+                // Animação 7: Bomba
                 case '7':
-                
+                    while(time < ANIMATION_TIME) {
+                        display_animation(
+                            bomb_data, 
+                            BOMB_FRAME_COUNT, 
+                            2000
+                        );
+                        time += 1000;
+                    }
                     break;
+
+                // Animação 8: esperando ser feita
                 case '8':
                 
                     break;
+
+                // Animação 9: esperando ser feita
                 case '9':
                 
                     break;
+
+                // Animação com buzzer: esperando ser feita
                 case '0':
                 
                     break;
+                
+                // Tecla A: apagar todos os leds
                 case 'A':
-                
+                    matrix_clear();
+                    matrix_write();
                     break;
-                case 'B':
-                
-                    break;
-                case 'C':
-                
-                    break;
-                case 'D':
-                
-                    break;
-                case '*':
-                
-                    break;
-                case '#':
 
+                // Tecla B: pinta tudo de azul intensidade 100%
+                case 'B':
+                    matrix_fill(0, 0, 255);
+                    matrix_write();
+                    break;
+
+                // Tecla B: pinta tudo de vermelho intensidade 80%
+                case 'C':
+                    matrix_fill(255 * 0.8, 0, 0);
+                    matrix_write();
+                    break;
+
+                // Tecla D: pinta tudo de verde intensidade 50%
+                case 'D':
+                    matrix_fill(0, 255 * 0.5, 0);
+                    matrix_write();
+                    break;
+
+                // Tecla #: pinta tudo de branco intensidade 20%
+                case '#':
+                    matrix_fill(255 *0.2 , 255 * 0.2, 255 * 0.2);
+                    matrix_write();
+                    break;
+
+                // Tecla *: limpa tudo e entra em modo bootsel
+                case '*':
+                    matrix_clear();
+                    matrix_write();
+                    reset_usb_boot(0, 0);
                     break;
             }
         }
